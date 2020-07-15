e3f70dc237b9d95bc1a2f429a009708115951bf7
srianth-Dev <srinathsri008@gmail.com>
2020-07-15 11:23:05 +0530

    Face Recognition using Dlib

---
+++ dlib/src/main/java/com/tzutalin/dlib/Constants.java
@@ -0,0 +1,34 @@
+package com.tzutalin.dlib;
+
+import android.os.Environment;
+
+import java.io.File;
+
+
+
+public final class Constants {
+    private Constants() {
+        // Constants should be prive
+    }
+
+    public static String getDLibDirectoryPath() {
+        File sdcard = Environment.getExternalStorageDirectory();
+        String targetPath = sdcard.getAbsolutePath() + File.separator + "dlib_rec_example";
+        return targetPath;
+    }
+
+    public static String getDLibImageDirectoryPath() {
+        String targetPath = getDLibDirectoryPath()+ File.separator + "images";
+        return targetPath;
+    }
+
+    public static String getFaceShapeModelPath() {
+        String targetPath = getDLibDirectoryPath() + File.separator + "shape_predictor_5_face_landmarks.dat";
+        return targetPath;
+    }
+
+    public static String getFaceDescriptorModelPath() {
+        String targetPath = getDLibDirectoryPath() + File.separator + "dlib_face_recognition_resnet_model_v1.dat";
+        return targetPath;
+    }
+}
